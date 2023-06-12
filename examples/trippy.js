359568784cefc3405ba9a2c90667418e0ba8655c
Lucas <88567622+LucasHT22@users.noreply.github.com>
2023-06-12 13:55:43 -0300

    Create trippy.js

---
+++ examples/trippy.js
@@ -0,0 +1,12 @@
+const t = new Turtle();
+
+for (let x = 0; x < +92; x++) {
+t.forward(182.8);
+t.arc(230, 250);
+t.right(-148);
+t.arc(220,250);
+t.left(241);
+t.forward(63.7);
+}
+
+drawTurtles(t);
