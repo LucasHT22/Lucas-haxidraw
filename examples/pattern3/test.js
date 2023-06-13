e24866472f5041c2f5e7c1ca0d562a4564fa11ee
Lucas <88567622+LucasHT22@users.noreply.github.com>
2023-06-13 14:06:00 -0300

    Create test.js

---
+++ examples/pattern3/test.js
@@ -0,0 +1,16 @@
+const t = new Turtle();
+
+for (let x = 42; x < +100; x++) {
+t.forward(53);
+t.arc(91, 9);
+t.left(-11);
+t.forward(80);
+t.left(980);
+t.forward(-4);
+t.right(82);
+t.arc(37,93);
+t.left(322);
+t.forward(-57);
+}
+
+drawTurtles(t);
